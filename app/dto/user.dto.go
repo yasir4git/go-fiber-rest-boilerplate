b231d689c7e937eabfa94d60e691c0c7dd77f56a
yasir4git <yasir4git@gmail.com>
2025-06-11 15:14:47 +0700

    fix: typo email

--- app/dto/user.dto.go
+++ app/dto/user.dto.go
@@ -11,14 +11,14 @@ type UserDTO struct {
 
 type CreateUserDTO struct {
 	Name        string  `json:"name"  validate:"required,min=3,max=32"`
-	Phone       string  `json:"email" validate:"required,phone"`
+	Phone       string  `json:"phone" validate:"required,phone"`
 	Email       *string `json:"email" validate:"email"`
 	CountryCode string  `json:"country_code" validate:"required"`
 }
 
 type UpdateUserDTO struct {
 	Name        string  `json:"name"  validate:"omitempty,min=3,max=32"`
-	Phone       string  `json:"email" validate:"required,phone"`
+	Phone       string  `json:"phone" validate:"required,phone"`
 	Email       *string `json:"email" validate:"email"`
 	CountryCode string  `json:"country_code" validate:"required"`
 }
